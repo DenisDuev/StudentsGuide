bacba5be16626255aac57ded521ec142a6561afa
Duev <denis.duev@sap.com>
2018-04-07 23:46:37 +0300

    add custom marker on the map

--- mockup/js/googlemap.js
+++ mockup/js/googlemap.js
@@ -1,7 +1,12 @@
 function myMap() {
+    var uluru = {lat: 42.674208, lng: 23.330210};
     var mapProp= {
-        center:new google.maps.LatLng(42.674208, 23.330210),
-        zoom:15,
+        center:uluru,
+        zoom:15
     };
     var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
 }
