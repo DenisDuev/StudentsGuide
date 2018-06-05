fe20d2fe8de01a0cebcde8a533c9c22f3b400295
Denis Duev <denis_duev@abv.bg>
2018-06-05 21:16:24 +0300

    add new functionality

---
+++ mockup/js/index.js
@@ -0,0 +1,113 @@
+var news = [
+    {
+        "title": "International Scientific Conference “The Balkans in European Integration –Its Past, Present and Future”",
+        "summary": "International Scientific Conference “The Balkans in European Integration –Its Past, Present and Future”Sofia University “St. Kliment Ohridski” was the host of “The Balkans in European Integration – Its Past, Present and Future”. In the two days’ forum scholars from Bulgaria and other European countries participated and traced the historical links between the Western Balkans and Europe, the integration processes in the European South-East as a phenomenon of the past and their perspective in view of the future accession of the region to the European Union.",
+        "content": "Amongst the participants were former prime ministers and ministers of foreign affairs of Serbia, Macedonia, Kosovo, Montenegro and Albania who were also prominent scholars from the Western Balkans. The group included Professor Zarko Korać, former prime minister of Serbia, Professor Vlado Bučkovski, former prime minister of the Republic of Macedonia, Associate Professor Bayram Kosumi, former prime minister of the Republic of Kosovo and Rector of the “Kadri Zeka” University in Gjilan, Professor Paskal Mico, former foreign minister and a minister for European integration of Albania.Organized on the initiative of the governing body of Sofia University “St. Kliment Ohridski” by the Department of the History of Byzantium and the Balkan Peoples at the Faculty of History of Sofia University, the conference aimed at supporting and enriching with scientific expertise the EU summit meeting EU – Western Balkans, which was being held in Sofia at the same time, by tracing the history, achievements and perspectives of the countries in the region and also accounting for the ever deepening relations with the European Union.",
+        "image": "news1.gif"
+    },
+    {
+        "title": "International Scientific Conference “The Balkans in European Integration –Its Past, Present and Future”",
+        "summary": "International Scientific Conference “The Balkans in European Integration –Its Past, Present and Future”Sofia University “St. Kliment Ohridski” was the host of “The Balkans in European Integration – Its Past, Present and Future”. In the two days’ forum scholars from Bulgaria and other European countries participated and traced the historical links between the Western Balkans and Europe, the integration processes in the European South-East as a phenomenon of the past and their perspective in view of the future accession of the region to the European Union.",
+        "content": "Amongst the participants were former prime ministers and ministers of foreign affairs of Serbia, Macedonia, Kosovo, Montenegro and Albania who were also prominent scholars from the Western Balkans. The group included Professor Zarko Korać, former prime minister of Serbia, Professor Vlado Bučkovski, former prime minister of the Republic of Macedonia, Associate Professor Bayram Kosumi, former prime minister of the Republic of Kosovo and Rector of the “Kadri Zeka” University in Gjilan, Professor Paskal Mico, former foreign minister and a minister for European integration of Albania.Organized on the initiative of the governing body of Sofia University “St. Kliment Ohridski” by the Department of the History of Byzantium and the Balkan Peoples at the Faculty of History of Sofia University, the conference aimed at supporting and enriching with scientific expertise the EU summit meeting EU – Western Balkans, which was being held in Sofia at the same time, by tracing the history, achievements and perspectives of the countries in the region and also accounting for the ever deepening relations with the European Union.",
+        "image": "news1.gif"
+    },
+    {
+        "title": "International Scientific Conference “The Balkans in European Integration –Its Past, Present and Future”",
+        "summary": "International Scientific Conference “The Balkans in European Integration –Its Past, Present and Future”Sofia University “St. Kliment Ohridski” was the host of “The Balkans in European Integration – Its Past, Present and Future”. In the two days’ forum scholars from Bulgaria and other European countries participated and traced the historical links between the Western Balkans and Europe, the integration processes in the European South-East as a phenomenon of the past and their perspective in view of the future accession of the region to the European Union.",
+        "content": "Amongst the participants were former prime ministers and ministers of foreign affairs of Serbia, Macedonia, Kosovo, Montenegro and Albania who were also prominent scholars from the Western Balkans. The group included Professor Zarko Korać, former prime minister of Serbia, Professor Vlado Bučkovski, former prime minister of the Republic of Macedonia, Associate Professor Bayram Kosumi, former prime minister of the Republic of Kosovo and Rector of the “Kadri Zeka” University in Gjilan, Professor Paskal Mico, former foreign minister and a minister for European integration of Albania.Organized on the initiative of the governing body of Sofia University “St. Kliment Ohridski” by the Department of the History of Byzantium and the Balkan Peoples at the Faculty of History of Sofia University, the conference aimed at supporting and enriching with scientific expertise the EU summit meeting EU – Western Balkans, which was being held in Sofia at the same time, by tracing the history, achievements and perspectives of the countries in the region and also accounting for the ever deepening relations with the European Union.",
+        "image": "news1.gif"
+    },
+    {
+        "title": "International Scientific Conference “The Balkans in European Integration –Its Past, Present and Future”",
+        "summary": "International Scientific Conference “The Balkans in European Integration –Its Past, Present and Future”Sofia University “St. Kliment Ohridski” was the host of “The Balkans in European Integration – Its Past, Present and Future”. In the two days’ forum scholars from Bulgaria and other European countries participated and traced the historical links between the Western Balkans and Europe, the integration processes in the European South-East as a phenomenon of the past and their perspective in view of the future accession of the region to the European Union.",
+        "content": "Amongst the participants were former prime ministers and ministers of foreign affairs of Serbia, Macedonia, Kosovo, Montenegro and Albania who were also prominent scholars from the Western Balkans. The group included Professor Zarko Korać, former prime minister of Serbia, Professor Vlado Bučkovski, former prime minister of the Republic of Macedonia, Associate Professor Bayram Kosumi, former prime minister of the Republic of Kosovo and Rector of the “Kadri Zeka” University in Gjilan, Professor Paskal Mico, former foreign minister and a minister for European integration of Albania.Organized on the initiative of the governing body of Sofia University “St. Kliment Ohridski” by the Department of the History of Byzantium and the Balkan Peoples at the Faculty of History of Sofia University, the conference aimed at supporting and enriching with scientific expertise the EU summit meeting EU – Western Balkans, which was being held in Sofia at the same time, by tracing the history, achievements and perspectives of the countries in the region and also accounting for the ever deepening relations with the European Union.",
+        "image": "news1.gif"
+    }
+];
+
+var universities = [
+    {
+        "name": "Sofia University Kliment Ohridski",
+        "description": "Sofia University St. Kliment Ohridski is the first Bulgarian high academic school establishment. Its history is an embodiment and a continuation of the centurylong cultural and educational tradition of the Bulgarian nation.In 1880 the Bulgarian Ministry of Education tabled a draft of the Main Educational Act for the Schools in the Principality of Bulgaria in the National Assembly. It envisaged that “after completing the requirements of the Real and Classical schools to open a Bulgarian high school (a University) that will teach the sciences of law, the arts, the sciences, the medical sciences, and the technological sciences.” In 1887, T. Ivanchev, minister of education, issued an Ordinance for the Opening of a Pedagogy Class Affiliated to the First Male Gymnasium in Sofia. The classes began on 1st. Oct., 1888. On 8th, Dec., 1888 the National Assembly passed an act transforming the latter into a High School; the act was ",
+        "image": "news1.gif"
+    },
+    {
+        "name": "Sofia University Kliment Ohridski",
+        "description": "Sofia University St. Kliment Ohridski is the first Bulgarian high academic school establishment. Its history is an embodiment and a continuation of the centurylong cultural and educational tradition of the Bulgarian nation.In 1880 the Bulgarian Ministry of Education tabled a draft of the Main Educational Act for the Schools in the Principality of Bulgaria in the National Assembly. It envisaged that “after completing the requirements of the Real and Classical schools to open a Bulgarian high school (a University) that will teach the sciences of law, the arts, the sciences, the medical sciences, and the technological sciences.” In 1887, T. Ivanchev, minister of education, issued an Ordinance for the Opening of a Pedagogy Class Affiliated to the First Male Gymnasium in Sofia. The classes began on 1st. Oct., 1888. On 8th, Dec., 1888 the National Assembly passed an act transforming the latter into a High School; the act was ",
+        "image": "news1.gif"
+    },
+    {
+        "name": "Sofia University Kliment Ohridski",
+        "description": "Sofia University St. Kliment Ohridski is the first Bulgarian high academic school establishment. Its history is an embodiment and a continuation of the centurylong cultural and educational tradition of the Bulgarian nation.In 1880 the Bulgarian Ministry of Education tabled a draft of the Main Educational Act for the Schools in the Principality of Bulgaria in the National Assembly. It envisaged that “after completing the requirements of the Real and Classical schools to open a Bulgarian high school (a University) that will teach the sciences of law, the arts, the sciences, the medical sciences, and the technological sciences.” In 1887, T. Ivanchev, minister of education, issued an Ordinance for the Opening of a Pedagogy Class Affiliated to the First Male Gymnasium in Sofia. The classes began on 1st. Oct., 1888. On 8th, Dec., 1888 the National Assembly passed an act transforming the latter into a High School; the act was ",
+        "image": "news1.gif"
+    },
+    {
+        "name": "Sofia University Kliment Ohridski",
+        "description": "Sofia University St. Kliment Ohridski is the first Bulgarian high academic school establishment. Its history is an embodiment and a continuation of the centurylong cultural and educational tradition of the Bulgarian nation.In 1880 the Bulgarian Ministry of Education tabled a draft of the Main Educational Act for the Schools in the Principality of Bulgaria in the National Assembly. It envisaged that “after completing the requirements of the Real and Classical schools to open a Bulgarian high school (a University) that will teach the sciences of law, the arts, the sciences, the medical sciences, and the technological sciences.” In 1887, T. Ivanchev, minister of education, issued an Ordinance for the Opening of a Pedagogy Class Affiliated to the First Male Gymnasium in Sofia. The classes began on 1st. Oct., 1888. On 8th, Dec., 1888 the National Assembly passed an act transforming the latter into a High School; the act was ",
+        "image": "news1.gif"
+    },
+    {
+        "name": "Sofia University Kliment Ohridski",
+        "description": "Sofia University St. Kliment Ohridski is the first Bulgarian high academic school establishment. Its history is an embodiment and a continuation of the centurylong cultural and educational tradition of the Bulgarian nation.In 1880 the Bulgarian Ministry of Education tabled a draft of the Main Educational Act for the Schools in the Principality of Bulgaria in the National Assembly. It envisaged that “after completing the requirements of the Real and Classical schools to open a Bulgarian high school (a University) that will teach the sciences of law, the arts, the sciences, the medical sciences, and the technological sciences.” In 1887, T. Ivanchev, minister of education, issued an Ordinance for the Opening of a Pedagogy Class Affiliated to the First Male Gymnasium in Sofia. The classes began on 1st. Oct., 1888. On 8th, Dec., 1888 the National Assembly passed an act transforming the latter into a High School; the act was ",
+        "image": "news1.gif"
+    }
+];
+
+function addAllNews() {
+    var index = 0;
+    var newDiv = document.getElementById("first");
+    for (var i = 0; i < news.length; i++) {
+        var data = news[i];
+        newDiv.appendChild(createNewsTile(index, data.image, data.summary, data.title));
+    }
+}
+
+function createNewsTile(iId, sImage, sDescription, sTitle) {
+    var p = document.createElement("p");
+    p.innerText = sDescription;
+    var image = document.createElement("img");
+    image.src = "images/" + sImage;
+    image.className = "in-tile";
+    var title = document.createElement("h3");
+    title.innerText = sTitle;
+    var div = document.createElement("div");
+    div.className = "horizontal-block";
+
+    div.appendChild(image);
+    div.appendChild(title);
+    div.appendChild(p);
+
+    var aTag = document.createElement("a");
+    aTag.href = "news.html?id=" + iId;
+    aTag.appendChild(div);
+    return aTag;
+}
+
+function addAllUniversities() {
+    var index = 0;
+    var universityDiv = document.getElementById("second");
+    for (var i = 0; i < universities.length; i++) {
+        var data = universities[i];
+        universityDiv.appendChild(createUniversityTile(index, data.image, data.name));
+    }
+}
+
+function createUniversityTile(sId, sImage, sName) {
+    var name = document.createElement("span");
+    name.style = "color: black";
+    name.innerText = sName;
+    var imageU = document.createElement("img");
+    imageU.src = "images/" + sImage;
+    imageU.className = "in-square";
+
+    var div = document.createElement("div");
+    div.className = "right-cube";
+    div.appendChild(imageU);
+    div.appendChild(name);
+
+    var aTag = document.createElement("a");
+    aTag.href="university.html?id=" + sId;
+    aTag.appendChild(div);
+
+    return aTag;
+}
