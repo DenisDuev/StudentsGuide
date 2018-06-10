e22c41fb9710aa22edbe48bbee3d10dd0b217d47
Denis Duev <denis_duev@abv.bg>
2018-06-10 11:11:11 +0300

    add content for universities, news and groups

--- mockup/js/groupModels.js
+++ mockup/js/groupModels.js
@@ -1,87 +1,145 @@
 var groups = [
     {
-        name: "Discretni Strukturi",
-        year: "2",
-        specialty: "SI",
+        name: "Preparation for interviews",
+        year: "3",
+        specialty: "Elective subjects",
         posts: [
             {
-                title: "New book",
-                content: "At thursday morning a new book about discrete mathematics will be presented.",
+                title: "Test Scores",
+                content: "All tests are already checked and mapped. If someone does not see the result, let me write. You will be able to see them tomorrow at the lecture.",
                 image: "su.gif"
             },
             {
-                title: "New book",
-                content: "At thursday morning a new book about discrete mathematics will be presented.",
+                title: "The Final Exam",
+                content: "The final exam will be on the 5th of June (Tuesday), from 18:00 in Hall 210 at the Chemical Faculty.",
                 image: "su.gif"
             },
             {
-                title: "New book",
-                content: "At thursday morning a new book about discrete mathematics will be presented.",
+                title: "Without Exercises",
+                content: "I remind you that tomorrow is a non-academic day and we will not have any classes. Also, on Wednesday, we will not have any more exercises again (as low attendance is even lower in the holidays).",
                 image: "su.gif"
             },
             {
-                title: "New book",
-                content: "At thursday morning a new book about discrete mathematics will be presented.",
+                title: "For Java Writers",
+                content: "In order to accept your solution, you should not have a package declaration in the source.",
+                image: "su.gif"
+            },
+            {
+                title: "We'll Have Exercises",
+                content: "Today Rally will not be able to perform the exercises, so I will replace her! As always, from 19:15 in the 321st room.",
                 image: "su.gif"
             }
         ]
     },
     {
-        name: "Discretni Strukturi 2",
+        name: "Operating Systems",
         year: "2",
         specialty: "SI",
         posts: [
             {
-                title: "New book",
-                content: "At thursday morning a new book about discrete mathematics will be presented.",
+                title: "Moved exercises 25.05 (Friday)",
+                content: "Practice will be held on Wednesday (06.06) from 19:00 in Hall 306. \n The exercise will be held Thursday (07.06) from 19:00 in Hall 321.",
                 image: "su.gif"
             },
             {
-                title: "New book",
-                content: "At thursday morning a new book about discrete mathematics will be presented.",
+                title: "Consultation",
+                content: "Hello, \n This Friday (13.04) from 16:00 in room 101 will be held a consultation on the OS for the first checkpoint. \n Greetings, \n Anton",
                 image: "su.gif"
             },
             {
-                title: "New book",
-                content: "At thursday morning a new book about discrete mathematics will be presented.",
+                title: "OS - practice, schedule",
+                content: "Hello, \n The course 'Operating Systems - Practice' will take place next week. \n greetings, \n --velin",
                 image: "su.gif"
             },
             {
-                title: "New book",
-                content: "At thursday morning a new book about discrete mathematics will be presented.",
+                title: "Additional Exercise for 6gr.(Volume 3)",
+                content: "May 23, 2018, 5:00 pm, Hall 320",
                 image: "su.gif"
             }
         ]
     },
     {
-        name: "Discretni Strukturi 3",
+        name: "Web Technology",
+        year: "3",
+        specialty: "SI",
+        posts: [
+            {
+                title: "Documentation Template",
+                content: "Hello colleagues,\n I now see that I've uploaded the documentation template to the uploader link - you can find it here: https://learn.fmi.uni-sofia.bg/mod/assign/view.php?id=102757. (the last link in the moodle). \n Greetings and success!",
+                image: "su.gif"
+            },
+            {
+                title: "Change time",
+                content: "Hello,\n Today we will start at 18:30 in the same room - 013. \n Greetings,\n Stanislav",
+                image: "su.gif"
+            },
+            {
+                title: "5th group - Without exercises today (17.04)",
+                content: "For health reasons, today I will not be able to do the exercises. See you on 24.04.",
+                image: "su.gif"
+            },
+            {
+                title: "4 hours lecture tomorrow on web technologies",
+                content: "Hello colleagues,\n Tomorrow Prof. Stefanov will be absent I will take 4 hours of lecture on web technologies, anyway holidays come and we may miss sometime or we have to be absent so as not to delay. (from 9:15 to 11 and from 11:15 to 13). \n Greetings, \n Milen",
+                image: "su.gif"
+            },
+            {
+                title: "Without exercises 28.04",
+                content: "Hello, \n There will be no exercise this Saturday. Also, the CSS-1 reviews may be delayed for a few days, which I apologize for. \n Sincerely, \n Ilia Yachev",
+                image: "su.gif"
+            }
+        ]
+    },
+    {
+        name: "Object-Oriented Programming",
+        year: "1",
+        specialty: "SI",
+        posts: [
+            {
+                title: "4th group - Without exercises today (19.12)",
+                content: " We will not have exercises this week (as low attendance is even lower in the holidays).",
+                image: "su.gif"
+            },
+            {
+                title: "Homework 2",
+                content: "Hello,\n Due to lagging with the material, we extend the second homework by one week. \n Greetings, \n Petyr",
+                image: "su.gif"
+            },
+            {
+                title: "Moved Exercises 25.10",
+                content: "The exercise will be held Tuesday (07.11) from 18:00 in Hall 306.",
+                image: "su.gif"
+            },
+            {
+                title: "Projects",
+                content: "Hello colleagues,\n The projects protection will start next week (15.01). \n Greetings, \n Petyr",
+                image: "su.gif"
+            }
+        ]
+    },
+    {
+        name: "Data Structures And Algorithms",
         year: "2",
         specialty: "SI",
         posts: [
             {
-                title: "New book",
-                content: "At thursday morning a new book about discrete mathematics will be presented.",
+                title: "Projects",
+                content: " You need to implement the data structures  you use in your project. \n Greetings,\n Stanislav",
                 image: "su.gif"
             },
             {
-                title: "New book",
-                content: "At thursday morning a new book about discrete mathematics will be presented.",
+                title: "Homework 1",
+                content: "Hello,\n For the first home you can use STL implementations. \n Greetings,\n Stanislav",
                 image: "su.gif"
             },
             {
-                title: "New book",
-                content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc hendrerit, tortor sagittis posuere\n" +
-                "                convallis, arcu est imperdiet tortor, sit amet ornare mi nulla eu mi. Aliquam dignissim quam nisi, vel\n" +
-                "                sagittis neque volutpat non. Sed quis tempus metus, eget pellentesque ex. Quisque venenatis ac libero in\n" +
-                "                condimentum",
+                title: "Lectures",
+                content: "This week (22.12) we will not have a lecture. \n Greetings \n Nora",
                 image: "su.gif"
             },
             {
-                title: "New book",
-                content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc hendrerit, tortor sagittis posuere\n" +
-                "                convallis, arcu est imperdiet tortor, sit amet ornare mi nulla eu mi. Aliquam dignissim quam nisi, vel\n" +
-                "                sagittis neque volutpat non. Sed quis tempus metus, eget pellentesque ex. Quisque venenatis ac libero in\n" +
-                "                condimentum",
+                title: "Last Homework",
+                content: "Hello colleagues,\n The last homework is gone. Successful session. \n Greetings, \n Stanislav",
                 image: "su.gif"
             }
         ]
